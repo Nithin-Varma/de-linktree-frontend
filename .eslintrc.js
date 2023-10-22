eda463648e6b1d7b8fe6c0c1c6dfd09597fda457
Nithin-Varma <mnithin1422@gmail.com>
2023-10-22 19:22:57 +0530

    fix: lint and some lib changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ["sznm/react", "plugin:react/jsx-runtime"],
+  rules: {
+    'prettier/prettier': ['error', {
+      endOfLine: 'auto'
+    }],
+  },
 };
