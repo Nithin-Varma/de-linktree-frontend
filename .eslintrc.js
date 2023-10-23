74d5f5dd550032c068e11944eb29708cba631f20
Nithin-Varma <mnithin1422@gmail.com>
2023-10-23 14:52:55 +0530

    remove existing repo

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: ["sznm/react", "plugin:react/jsx-runtime"],
-  rules: {
-    'prettier/prettier': ['error', {
-      endOfLine: 'auto'
-    }],
-  },
-};
